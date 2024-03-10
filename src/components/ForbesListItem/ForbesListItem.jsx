db501b89a393f43f14b5d5fcfee4dbde3117bb50
wordisstuff <wordisstuff@gmail.com>
2024-03-10 06:05:55 -0500

    task3 done v3.

--- src/components/ForbesListItem/ForbesListItem.jsx
+++ src/components/ForbesListItem/ForbesListItem.jsx
@@ -1,6 +1,16 @@
-// import { BiDollarCircle } from 'react-icons/bi';
-// import { FcBullish, FcBearish } from 'react-icons/fc';
+import { BiDollarCircle } from 'react-icons/bi';
+import { FcBullish, FcBearish } from 'react-icons/fc';
+import style from './ForbesListItem.module.css';
 
-export const ForbesListItem = () => {
-  return <h3>ForbesListItem</h3>;
+export const ForbesListItem = ({ name, capital, avatar, isIncrease }) => {
+  return (
+    <>
+      <img className={style.avatar} src={avatar} alt={name} />
+      <h3 className={style.title}>{name}</h3>
+      <span className={style.capital}>
+        {capital} <BiDollarCircle color="#2196f3" size={22} />
+        {isIncrease ? <FcBullish /> : <FcBearish />}
+      </span>
+    </>
+  );
 };
