fc966824bed849485fb85f69a7cc8d2dd98cd825
SergeyPanchenko72 <143348555+SergeyPanchenko72@users.noreply.github.com>
2024-04-07 12:12:01 +0300

    add task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,25 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
-
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+export const Statistics = ({ stats, title }) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>{title}</h3>}
+      <ul className={style.list}>
+        {stats.map((item, index) => (
+          <li className={style.item} key={item.id}>
+            <StatisticsItem {...item} icon={icons[index]} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
