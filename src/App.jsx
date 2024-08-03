3d871e848234a70f1a584a01c7025b4f258e0258
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-08-03 15:34:38 +0300

    task 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   ForbesList,
 } from 'components';
 
+import stats from 'data/stats.json';
+
 import article from 'data/article.json';
 
 export const App = () => {
@@ -16,13 +18,10 @@ export const App = () => {
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
         <BlogCard {...article} />
-
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
-
+        <Statistics title="Main Statistics" stats={stats} />
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
-
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
       </Container>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,28 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+const icons = [
+  <FaRegThumbsUp key="1" />,
+  <MdPeople key="2" />,
+  <MdOutlineProductionQuantityLimits key="3" />,
+  <GiTreeDoor key="4" />,
+];
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>Main Statistics</h3>}
+
+      <ul className={style.list}>
+        {stats.map((st, idx) => (
+          <li key={st.id} className={style.item}>
+            <StatisticsItem stat={st} icon={icons[idx]} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
