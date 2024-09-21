5e5620f863c34fbdcf2b3f2c7efb488c52a6d109
YuraPerebzyak <yura.perebzyak.231998@gmail.com>
2024-09-21 12:28:44 +0300

    add task2

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   ForbesList,
 } from 'components';
 
+import stats from 'data/stats.json'
 import article from 'data/article.json';
 
 export const App = () => {
@@ -26,7 +27,7 @@ export const App = () => {
         />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,14 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import style from './StatisticsItem.module.css';
+
+export const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'blue', size: '30px' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
