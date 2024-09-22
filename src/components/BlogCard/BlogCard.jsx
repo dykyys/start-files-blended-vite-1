1b465dc8de2911a0a1171fc71177d47484658ec3
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-09-22 14:23:15 +0200

    add task

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,32 @@
-export const BlogCard = () => {
-  return <h2>BlogCard</h2>;
+import styles from './BlogCard.module.css';
+export const BlogCard = ({
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img className={styles.cardPoster} src={poster} alt={tag} />
+      </div>
+      <div className={styles.cardBody}>
+        <span className={styles.tag}>{tag}</span>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardText}>{description}</p>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.userBox}>
+          <img className={styles.avatar} src={avatar} alt={name} />
+          <div>
+            <h3 className={styles.userName}>{name}</h3>
+            <small className={styles.date}>{postedAt}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
