009c1e368e0976998a3de7bcdbdfefde40c31017
Maksym Adamenko <maimav56@gmail.com>
2024-11-16 16:26:38 +0200

    Task 1 done

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,34 @@
-export const BlogCard = () => {
-  return <h2>BlogCard</h2>;
+import { formatDateToNow } from 'helpers/formatDate';
+import styles from './BlogCard.module.css';
+
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img className={styles.cardPoster} src={poster} alt={title} />
+      </div>
+      <div className={styles.cardBody}>
+        <span className={styles.tag}>{tag}</span>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardText}>{description}</p>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.userBox}>
+          <img className={styles.avatar} src={avatar} alt={userName} />
+          <div>
+            <h3 className={styles.userName}>{userName}</h3>
+            <small className={styles.date}>{formatDateToNow(postedAt)}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
