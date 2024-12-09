a239125da5657290d022b310a0bf96502fb24a41
Sergii <dykyy.s@gmail.com>
2024-12-09 13:03:40 +0200

    update Heading

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,16 +1,16 @@
 import styles from './Heading.module.css';
 import clsx from 'clsx';
 
-const Heading = ({ title, top, bottom }) => {
+const Heading = ({ title, top, bottom, tag: Tag = 'h2' }) => {
   return (
-    <h2
+    <Tag
       className={clsx(styles.title, {
         [styles.top]: top,
         [styles.bottom]: bottom,
       })}
     >
       {title}
-    </h2>
+    </Tag>
   );
 };
 
