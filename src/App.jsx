9e3f19dba8ee604e1b0fde88aaa83cbf34775468
Dmytrotarn <112310528+Dmytrotarn@users.noreply.github.com>
2024-12-15 16:00:10 +0200

    Task-3 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ForbesList from './components/ForbesList/ForbesList';
 import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
+import forbes from './data/forbes.json';
 
 export const App = () => {
   return (
@@ -17,7 +18,7 @@ export const App = () => {
         <Statistics title="Main Statistics" />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
