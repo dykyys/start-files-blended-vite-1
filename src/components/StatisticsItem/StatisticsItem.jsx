cb60a8b3688831e303e7c2f3aa281f3100c5f303
Andrij P. <andrejpopkov565@gmail.com>
2024-12-15 11:59:00 +0300

    task-4

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,16 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import statsData from '../../data/stats.json';
+import styleStList from './StatisticsItem.module.css';
+
+const StatisticsItem = ({ id, title, total, icon: Icon }) => {
+  return (
+    <>
+      <li className={styleStList.item} key={id}>
+        <Icon size={24} />
+        <span className={styleStList.counter}>{total}</span>
+        <p className={statsData.text}>{title}</p>
+      </li>
+    </>
+  );
 };
 
 export default StatisticsItem;
