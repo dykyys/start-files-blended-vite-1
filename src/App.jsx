5d5494dac388c8742695a15c5cdb9dd37a3f44d0
MashaSavchuk <savchuk_maria@ukr.net>
2025-02-01 14:32:55 +0000

    task 2 compleated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 import article from './data/article.json';
+import stats from './data/stats.json';
 
 export const App = () => {
   return (
@@ -21,9 +22,8 @@ export const App = () => {
           avatar={article.avatar}
           postedAt={article.postedAt}
         />
-
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
         <Heading title="Task 4 Crypto history" top bottom />
